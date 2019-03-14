b39ad49888d2ad408070e265bc4a0ad7fbd8a3d2
Juhana Jauhiainen <juhana.jauhiainen@gmail.com>
2019-03-14 23:27:21 +0200

    Added vCard fields

--- example/src/App.js
+++ example/src/App.js
@@ -4,14 +4,30 @@ import ContactCard from "react-contactcard";
 
 export default class App extends Component {
   render() {
+    const phonenumbers = [
+      {
+        type: "WORK",
+        value: "+555 4321"
+      }
+    ];
+
+    const addresses = [
+      {
+        type: "WORK",
+        value: "1234 Livingston Street, NY, USA"
+      }
+    ]
+
     return (
       <div style={{maxWidth: 500}}>
         <ContactCard
           fullName="Abraham Lincoln"
           title="Boxer, President of the United States"
           email="abe@whitehouse.gov"
-          phone="+555 4321"
-          profilePicture="abraham-lincoln.jpg"
+          phonenumbers={phonenumbers}
+          addresses={addresses}
+          organization="United States Goverment"
+          photo="abraham-lincoln.jpg"
         />
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -7,25 +7,45 @@ export default class ContactCard extends Component {
   static propTypes = {
     fullName: PropTypes.string,
     title: PropTypes.string,
-    phone: PropTypes.string,
-    email: PropTypes.string,
-    profilePicture: PropTypes.string
+    organization: PropTypes.string,
+    photo: PropTypes.string,
+    phonenumbers: PropTypes.array,
+    addresses: PropTypes.array,
+    email: PropTypes.string
   };
 
   render() {
-    const { fullName, title, phone, email, profilePicture } = this.props;
+    const { fullName, title, organization, phonenumbers, addresses, email, photo} = this.props;
+
+    const tel = phonenumbers.map(phone => {
+      return (
+        <h2>
+          {phone.type} {phone.value}
+        </h2>
+      );
+    });
+
+    const addr = addresses.map(address => {
+      return (
+        <h2>
+          {address.type} {address.value}
+        </h2>
+      );
+    });
 
     return (
       <div className={styles.card}>
         <div className={styles.profilePicture}>
-          <img src={profilePicture} />
+          <img src={photo} />
         </div>
         <div className={styles.content}>
           <div className={styles.icons} />
           <h1>{fullName}</h1>
           <h2>{title}</h2>
+          <h2>{organization}</h2>
           <h2>{email}</h2>
-          <h2>{phone}</h2>
+          {tel}
+          {addr}
         </div>
       </div>
     );
